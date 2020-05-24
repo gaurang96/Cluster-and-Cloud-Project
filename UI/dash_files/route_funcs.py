3f7223ed20884f128ac755ac6cd3bb949eb41755
Jack <crellinj@student.unimelb.edu.au>
2020-05-24 19:50:09 +1000

    minor updates

--- UI/dash_files/route_funcs.py
+++ UI/dash_files/route_funcs.py
@@ -5,3 +5,17 @@ import json
 def dump_data():
 	return json.dumps("everything is a-ok")
 
+
+def dump_aurin(database):
+	database.connect()
+	view_data = database.collect_view('aurin', 'view_1')
+
+	j_data = view_data #perform any transformations needed beforehand
+	return json.dumps(j_data)
+
+def dump_twitter(database):
+	database.connect()
+	view_data = database.collect_view('twitter', 'view_1')
+
+	j_data = view_data #perform any transformations needed beforehand
+	return json.dumps(j_data)
