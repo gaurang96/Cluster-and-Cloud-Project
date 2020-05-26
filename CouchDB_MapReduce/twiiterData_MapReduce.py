641c693af745c83675c9a7c653dabcedc0932739
Gaurang Sharma <40225761+gaurang96@users.noreply.github.com>
2020-05-26 10:27:49 +1000

    Update and rename twiiterData_MapReduce.txt to twiiterData_MapReduce.py

---
+++ CouchDB_MapReduce/twiiterData_MapReduce.py
@@ -0,0 +1,46 @@
+#Twitter Data map Reduce functions
+# 3 Views were created for the tweets using 3 different functions MapReduce functions which are given below.
+
+from cloudant.client import Cloudant
+from cloudant.error import CloudantException
+from cloudant.result import Result, ResultByKey
+from cloudant import design_document
+serviceUsername = "admin"
+servicePassword = "123"
+serviceURL = "http://admin:123@172.26.132.96:5984/"
+
+client = Cloudant(serviceUsername, servicePassword, url=serviceURL)
+client.connect()
+my_database = client['twitterdata']
+
+#----------------------------------------------------
+
+view = design_document.DesignDocument(my_database,document_id='view')
+ddoc.add_view('myview',"""function (doc) {
+  if (doc.sentiment.compound > 0.05){
+  emit([doc.sentiment.compound, doc.location], 1);
+}
+}""")
+view.save()
+
+#----------------------------------------------------
+
+view2 = design_document.DesignDocument(my_database,document_id='view2')
+view2.add_view('view2',"""function (doc) {
+  if (doc.sentiment.compound < -0.05){
+  emit([doc.sentiment.compound, doc.location], 1);
+}
+}""")
+view2.save()
+
+#----------------------------------------------------
+
+view3 = design_document.DesignDocument(my_database,document_id='view3')
+view3.add_view('view3',"""function (doc) {
+  if (doc.sentiment.compound > -0.05 && doc.sentiment.compound < 0.05){
+  emit([doc.sentiment.compound, doc.location], 1);
+}
+}""")
+view3.save()
+
+#----------------------------------------------------
